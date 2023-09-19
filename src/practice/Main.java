f77be98a596901a3a706372e73240f4cabaaa24a
Evgenii Tsapko <yevgeni.tsapko@gmail.com>
2023-09-19 15:59:31 +0200

    practice HW46

--- src/practice/Main.java
+++ src/practice/Main.java
@@ -1,67 +1,67 @@
+/**
+ * Пусть есть массив, принимающий три вида скобок : '(' ,')' ,'{', '}','[', ']'  ( 6 различных символов).
+ *     Нужно написать метод, определяющий является ли порядок
+ *     скобок в массиве корректным.
+ *     {}([]) - корректный порядок
+ *     {}([]{()}) - корректный порядок
+ *     {(] - некорректный порядок
+ *     {}([])} - некорректный порядок
+ *     {}([])( - некорректный
+ *     {(})
+ *     Использовать деку при решении
+
+
+    АРХИТЕКТУРА И ОСНОВНОЙ ПРИНЦИП ПРЕДЛАГАЕМОГО РЕШЕНИЯ
+
+    Основной метод:
+    public static boolean <КОРРЕКТНЫЕ-ли СКОБКИ В СТРОКЕ>(String str) {
+        Deque<Character> deqOfBrackets = new ArrayDeque<>();
+    for (char bracket : str.toCharArray()) { // предполагаемая скобка
+        if ( метод bool <это скобка открывающая?>) {
+            deqOfBrackets.push(bracket);
+        } else if (метод bool <это скобка закрывающая?>) {
+ (тогда удаляем  и проверяем 'парность\непарность'
+ только-что удалённой скобки с текущей )
+            if (deqOfBrackets.isEmpty() || ! метод bool <скобки корректно согласованы?(deqOfBrackets.pop(), bracket)>) {
+                return false;
+            }
+        }
+    }
+        return deqOfBrackets.isEmpty();
+    }
+ */
 package practice;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
 public class Main {
-    public static boolean isCorrectBrackets(String str) {
-        Deque<Character> deq = new ArrayDeque<>();
-
+    public static boolean isValidBrackets(String str) {
+        Deque<Character> deqOfBrackets = new ArrayDeque<>();
         for (char bracket : str.toCharArray()) {
             switch (bracket) {
                 case '(':
                 case '{':
                 case '[':
                 case '<':
-
-                    deq.push(bracket);
+                    deqOfBrackets.push(bracket);
                     break;
                 case ')':
                 case '}':
                 case ']':
                 case '>':
-                    if (deq.isEmpty() || !isCorrectMatch(deq.pop(), bracket)) {
+                    if (deqOfBrackets.isEmpty() ||
+                       !isCorrectMatch(deqOfBrackets.pop(), bracket)) {
                         return false;
                     }
                     break;
                 default:
-                    // Ignore other characters in the string
+                    // ignore any other chars
                     break;
             }
         }
 
-        return deq.isEmpty();
+        return deqOfBrackets.isEmpty();
     }
-//    public static boolean <КОРРЕКТНЫЕ-ли СКОБКИ В СТРОКЕ>(String str) {
-//        Deque<Character> D = new ArrayDeque<>();
-//    for (char bracket : brackets.toCharArray()) {
-//        if ( <метод скобка открывающая >) {
-//            D.push(bracket);
-//        } else if (<метод скобка закрывающая >) {
-//            if (D.isEmpty() || ! метод <скобки корректно согласованы (D.pop(), bracket)>) {
-//                return false;
-//            }
-//        }
-//    }
-//
-//        return D.isEmpty();
-//}
-
-
-
-    private static boolean isOpen(char bracket) {
-        return bracket == '(' ||
-               bracket == '{' ||
-               bracket == '<' ||
-               bracket == '[';
-    }
-
-    private static boolean isClose(char bracket) {
-        return bracket == ')' ||
-               bracket == '}' ||
-               bracket == '>' ||
-               bracket == ']';
-    }
-
     private static boolean isCorrectMatch(char open, char close) {
         return (open == '(' && close == ')') ||
                (open == '{' && close == '}') ||
@@ -69,15 +69,17 @@ public class Main {
                (open == '[' && close == ']');
     }
     public static void main(String[] args) {
-        String valid = "<({[d]})>";
-        String invalid = "({[})";
-        String invalid1 = "(<{[d])}";
-        String valid1 = "{(ss[d]ggg)}";
+        String Str0 = "({<[d];>})";
+        String Str1 = "({[}>);";
+        String Str2 = "(<{[d]);}";
+        String Str3 = "{(JS[i])}<iframe>";
+        String Str4 = "})JS]i[({>iframe<";
 
-        System.out.println("Valid brackets: " + isCorrectBrackets(valid)); // Выводит "Valid brackets: true"
-        System.out.println("Invalid brackets: " + isCorrectBrackets(invalid)); // Выводит "Invalid brackets: false"
-        System.out.println("Invalid brackets: " + isCorrectBrackets(invalid1));
-        System.out.println("Invalid brackets: " + isCorrectBrackets(valid1));
+        System.out.println("Validity of string '"+ Str0 + "' is:" + isValidBrackets(Str0));
+        System.out.println("Validity of string '"+ Str1 + "' is:" + isValidBrackets(Str1));
+        System.out.println("Validity of string '"+ Str2 + "' is:" + isValidBrackets(Str2));
+        System.out.println("Validity of string '"+ Str3 + "' is:" + isValidBrackets(Str3));
+        System.out.println("Validity of string '"+ Str4 + "' is:" + isValidBrackets(Str4));
     }
 
 }
